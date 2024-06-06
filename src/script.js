17a81f2ad6d6aef723c2aa5e3743eefa7ca875db
KatrinaIT <79994321+KatrinaIT@users.noreply.github.com>
2024-06-06 11:46:37 -0600

    added basic form handeling, added typewriter plugin

--- src/script.js
+++ src/script.js
@@ -1,1 +1,15 @@
-//alert("Hi there!");
+function generateQuote(event) {
+	event.preventDefault();
+
+	new Typewriter("#quote", {
+    strings: "La tombe dit à la rose",
+    autoStart: true,
+    delay: 1,
+    cursor: "",
+  });
+
+	console.log("Generating the Quote for you");
+}
+
+let poemFormElement = document.querySelector("#quote-gen-form");
+poemFormElement.addEventListener("submit", generateQuote);
