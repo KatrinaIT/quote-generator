0c2e0e4a77e71852661fe97fea1245648e6a3d19
KatrinaIT <79994321+KatrinaIT@users.noreply.github.com>
2024-06-06 12:56:10 -0600

    added AI API integration

--- src/script.js
+++ src/script.js
@@ -1,14 +1,26 @@
-function generateQuote(event) {
-	event.preventDefault();
-
-	new Typewriter("#quote", {
-    strings: "La tombe dit à la rose",
+function displayQuote(response) {
+  console.log("Quote was generated");
+  new Typewriter("#quote", {
+    strings: response.data.answer,
     autoStart: true,
     delay: 1,
     cursor: "",
   });
+}
 
-	console.log("Generating the Quote for you");
+function generateQuote(event) {
+  event.preventDefault();
+
+  let instructionsInput = document.querySelector("#user-instructions");
+  let apiKey = "1ad00cd57fae02a14f050bfc3o387tf2";
+  let prompt = `please generate the wisdom quote for the provided topic of ${instructionsInput.value}`;
+  let context =
+    "you are an experienced wisdom guru, life coach, who knows human psychology. you use old and modern wisdom sayings to teach people how to live life with fullfillment, joy, and gratitude. Who is productive and successful in each aspect of life. Sign the quote on the new line at the end inside the <strong> element with 'Your Wisdom Coach ClAI'";
+  let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiUrl).then(displayQuote);
+
+  console.log("Generating the Quote for you");
 }
 
 let poemFormElement = document.querySelector("#quote-gen-form");
