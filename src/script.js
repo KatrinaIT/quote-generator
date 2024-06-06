46bb4486e0db7f481582a1c2b0397e55a549e78e
KatrinaIT <79994321+KatrinaIT@users.noreply.github.com>
2024-06-06 13:33:34 -0600

    improved UX with hint and removed debugging code

--- src/script.js
+++ src/script.js
@@ -1,5 +1,5 @@
 function displayQuote(response) {
-  console.log("Quote was generated");
+
   new Typewriter("#quote", {
     strings: response.data.answer,
     autoStart: true,
@@ -15,12 +15,15 @@ function generateQuote(event) {
   let apiKey = "1ad00cd57fae02a14f050bfc3o387tf2";
   let prompt = `please generate the wisdom quote for the provided topic of ${instructionsInput.value}`;
   let context =
-    "you are an experienced wisdom guru, life coach, who knows human psychology. you use old and modern wisdom sayings to teach people how to live life with fullfillment, joy, and gratitude. Who is productive and successful in each aspect of life. Add at the end <br /> and on the new line 'Your Wisdom Coach ClAI' inside the <strong> element";
+    "you are an experienced wisdom guru, life coach, who knows human psychology. you use old and modern wisdom sayings to teach people how to live life with fullfillment, joy, and gratitude. Who is productive and successful in each aspect of life. Sign the quote with 'Your Wisdom Coach ClAI' on the new line after <br /> tag and inside a <strong> element at the end of the quote and NOT at the beginning";
+
   let apiUrl = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
-  axios.get(apiUrl).then(displayQuote);
+  let quoteElement = document.querySelector("#quote");
+  quoteElement.classList.remove("hidden");
+  quoteElement.innerHTML = `<div class="generating">⏳ Generating a quote about ${instructionsInput.value}</div>`;
 
-  console.log("Generating the Quote for you");
+  axios.get(apiUrl).then(displayQuote);
 }
 
 let poemFormElement = document.querySelector("#quote-gen-form");
